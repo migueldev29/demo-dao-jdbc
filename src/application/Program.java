4265313daa649a3be85f79f3ba398fc23b7c16f8
Miguel Domingues <migueldev29@gmail.com>
2023-10-21 09:38:02 -0300

    findById Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,11 +14,9 @@ public class Program {
 		
 		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 
